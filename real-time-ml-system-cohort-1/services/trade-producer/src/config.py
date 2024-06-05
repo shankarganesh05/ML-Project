c68898ca0775174c5b974e23babbaa811a28d58d
shankarganesh05 <shankarganesh.d@gmail.com>
2024-06-05 16:02:46 +0530

    Added Environmental Variable

--- real-time-ml-system-cohort-1/services/trade-producer/src/config.py
+++ real-time-ml-system-cohort-1/services/trade-producer/src/config.py
@@ -1,4 +1,8 @@
+import os
+from dotenv import load_dotenv,find_dotenv
 
-kafka_broker_address='redpanda-0:9092'
+load_dotenv(find_dotenv())
+
+kafka_broker_address=os.environ['KAFKA_BROKER_ADDRESS']
 kafka_topic_name='trade'
 product_id = ['BTC/USD','ETH/USD','BTC/GBP']
