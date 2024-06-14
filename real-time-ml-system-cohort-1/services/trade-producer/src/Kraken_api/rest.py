dbb599ff038e8ef76636dd498ab05f081d81666e
shankarganesh05 <shankarganesh.d@gmail.com>
2024-06-14 13:00:02 +0530

    Handeled Multiple Product in KrakenRest

--- real-time-ml-system-cohort-1/services/trade-producer/src/Kraken_api/rest.py
+++ real-time-ml-system-cohort-1/services/trade-producer/src/Kraken_api/rest.py
@@ -1,8 +1,43 @@
-from typing import List, Dict
 import json
+from typing import Dict, List,Tuple
+from datetime import datetime, timezone
+from time import sleep
 import requests
-
 from loguru import logger
+
+class KrakenRestAPIMultipleProducts:
+    def __init__(
+        self,
+        product_id: List[str],
+        last_n_days: int,
+    ) -> None:
+        self.product_id = product_id
+
+        self.kraken_apis = [
+            KrakenRestAPI(product_id=product_id, last_n_days=last_n_days)
+            for product_id in product_id
+        ]
+    def get_trades(self) -> List[Dict]:
+        trades: List[Dict] = []
+        for kraken_api in self.kraken_apis:
+            if kraken_api.is_done():
+                continue
+            else:
+                trades += kraken_api.get_trades()
+        return trades
+    
+    def is_done(self) -> bool:
+        """
+        Returns True if all kraken_apis in self.kraken_apis are done fetching historical.
+        It returns False otherwise.
+        """
+        for kraken_api in self.kraken_apis:
+            if not kraken_api.is_done():
+                return False
+            
+        return True
+                
+
 
 class KrakenRestAPI:
     URL = "https://api.kraken.com/0/public/Trades?pair={product_id}&since={since_sec}"
@@ -10,48 +45,62 @@ class KrakenRestAPI:
     def __init__(
             self,
             product_id:List[str],
-            from_ms:int,
-            to_ms:int,
+            #from_ms:int,
+            #to_ms:int,
+            last_n_days: int,
     )-> None:
         self.product_id = product_id
-        self.from_ms = from_ms
-        self.to_ms = to_ms
+        self.from_ms,self.to_ms = self._init_from_to_ms(last_n_days)
         self._is_done = False
-    
+        self.last_trade_ms = self.from_ms
+        
+    def _init_from_to_ms(self,last_n_days: int) -> Tuple[int, int]:
+        today_date = datetime.now(timezone.utc).replace(
+            hour=0, minute=0, second=0, microsecond=0
+        )
+        to_ms = int(today_date.timestamp() * 1000)
+        from_ms = to_ms - last_n_days * 24 * 60 * 60 * 1000
+        return from_ms, to_ms
+
+
     def get_trades(self)-> List[Dict]:
 
 
         payload = {}
         headers = {'Accept': 'application/json'}
 
-        since_sec = self.from_ms // 1000
-        url= self.URL.format(product_id = self.product_id[0],since_sec = since_sec)
+        since_sec = self.last_trade_ms // 1000
+        url= self.URL.format(product_id = self.product_id,since_sec = since_sec)
         response = requests.request("GET", url, headers=headers, data=payload)
 
         # parse string into dictionary
         data = json.loads(response.text)
 
         trades= []
-        for trade in data['result'][self.product_id[0]]:
+        for trade in data['result'][self.product_id]:
             trades.append({
                 'price': float(trade[0]),
                 'volume': float(trade[1]),
                 'time' : int(trade[2]),
-                'product_id': self.product_id[0]
+                'product_id': self.product_id
                 
             })
+        # filter out trades that are after the end timestamp
+        trades = [trade for trade in trades if trade['time'] <= self.to_ms // 1000]
         # check if we are done fetching historical data
         last_ts_in_ns = int(data['result']['last'])
         # convert nanoseconds to milliseconds
-        last_ts = last_ts_in_ns // 1_000_000
+        self.last_trade_ms = last_ts_in_ns // 1_000_000
         
-        if last_ts >= self.to_ms:
+        if self.last_trade_ms >= self.to_ms:
             # yes, we are done
             self._is_done = True
 
         logger.debug(f'Fetched {len(trades)} trades')
         # log the last trade timestamp
-        logger.debug(f'Last trade timestamp: {last_ts}')
+        logger.debug(f'Last trade timestamp: {self.last_trade_ms}')
+
+        sleep(1)
 
         return trades
     def is_done(self) -> bool:
--- real-time-ml-system-cohort-1/services/trade-producer/src/main.py
+++ real-time-ml-system-cohort-1/services/trade-producer/src/main.py
@@ -1,28 +1,28 @@
 # Create an Application instance with Kafka configs
-import time
-from time import sleep
+
+
 from typing import Dict, List
 
 from loguru import logger
 from quixstreams import Application
 
 from src import config
+from src.Kraken_api.rest import KrakenRestAPIMultipleProducts
 from src.Kraken_api.websocket import krakenWebSocketTradeApi
-from src.Kraken_api.rest import KrakenRestAPI
 
 
-def produce_trades(kafka_broker_address: str, kafka_topic_name: str,live_or_historical:str,last_n_days:int) -> None:
+def produce_trades(kafka_broker_address: str, kafka_topic_name: str,product_id:List[str],live_or_historical:str,last_n_days:int) -> None:
     app = Application(broker_address=kafka_broker_address)
 
     topic = app.topic(name=kafka_topic_name, value_serializer='json')
 
     # event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
     if live_or_historical == 'live':
-        kraken_api = krakenWebSocketTradeApi(product_id=config.product_id)
+        kraken_api = krakenWebSocketTradeApi(product_id=product_id)
     else:
-        to_ms = int(time.time()*1000)
-        from_ms = to_ms - last_n_days * 24 * 60 * 60 * 1000
-        kraken_api = KrakenRestAPI(product_id=config.product_id,from_ms=from_ms,to_ms=to_ms)
+        # to_ms = int(time.time()*1000)
+        # from_ms = to_ms - last_n_days * 24 * 60 * 60 * 1000
+        kraken_api = KrakenRestAPIMultipleProducts(product_id=product_id,last_n_days=last_n_days)
 
     logger.info('Creating the producer...')
     with app.get_producer() as producer:
@@ -36,10 +36,10 @@ def produce_trades(kafka_broker_address: str, kafka_topic_name: str,live_or_hist
             trades: List[Dict] = kraken_api.get_trades()
             for trade in trades:
                 # Serialize an event using the defined Topic
-                message = topic.serialize(key=trade['product_id'], value=trade)
+                message = topic.serialize(key=trade['product_id'], value=trade,timestamp_ms=trade['time'] * 1000)
 
                 # Produce a message into the Kafka topic
-                producer.produce(topic=topic.name, value=message.value, key=message.key)
+                producer.produce(topic=topic.name, value=message.value, key=message.key,timestamp=message.timestamp)
                 logger.info(trade)
         #sleep(1)
 
@@ -50,6 +50,7 @@ if __name__ == '__main__':
         produce_trades(
             kafka_broker_address=config.kafka_broker_address,
             kafka_topic_name=config.kafka_topic_name,
+            product_id = config.product_id,
             live_or_historical= config.live_or_historical,
             last_n_days= config.last_n_days
         )
